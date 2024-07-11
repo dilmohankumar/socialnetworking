6264436843e3af40df3a8bd9d2200abcc207c729
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-07-11 18:11:02 +0530

    1

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Route path="/contact" element={<Contact />} /> 
           
         </Routes>
-      <Footer/>
+      <Footer/>   
       </BrowserRouter>
     </div> 
   );
