baeeb4ddb82979c6872adfd2f9b004087faf0313
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-07-12 06:13:38 +0530

    dd

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Importing Routes and Route
+import Home from './pages/home/home';
+import Services from './pages/service/service'; // Changed 'Service' to 'Services' for consistency
+import Navbar from './components/Navbar/navbar'; // Added import statement for Navbar
+import Footer from './components/footer/footer'; // Added import statement for Footer
+import About from './pages/about/about';
+import Contact from './pages/contact/contact';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter> 
+      <Navbar/>
+        
+        <Routes>
+        <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={<Services />} /> 
+          <Route path="/contact" element={<Contact />} /> 
+          
+        </Routes>
+      <Footer/>   
+      </BrowserRouter> 
+    </div> 
+  );
+}
+
+export default App; 
